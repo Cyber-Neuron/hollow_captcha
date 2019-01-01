9703b35a762eeca19ef63fee553a59213971155d
Dan <cybernerves@outlook.com>
2019-01-01 01:35:28 -0500

    init

---
+++ generator.py
@@ -0,0 +1,48 @@
+'''
+Created on Dec 30, 2018
+
+
+'''
+
+
+from image import WheezyCaptcha
+import fire
+import random
+import os
+#_characters = 'adefhjkmnprstuvwxyABDEFHJKLMNPRSTUVWXY34578'
+_characters = 'ABCDEFGHJKLMNPRSTUVWXY3456789'
+#"345689abcdeghjkmnprstuvwxy"
+
+
+    
+def gen(amount,repeat=10,fonts_dir="fonts/",out_dir="train_samples/"):
+    files = os.listdir(fonts_dir)
+    fonts=[]
+    for name in files:
+        if ".ttf" in name:
+            fonts.append(fonts_dir+name)
+    image = WheezyCaptcha(fonts=fonts,
+                        font_sizes=[90],
+                       squeeze_factor=0.5,
+                       border=2
+                       
+                       )
+    # im = image.generate_dbg('3A9B')
+    for _ in range(amount):
+        sizes = [4, 5]
+        chars = random.sample(_characters, random.choice(sizes))
+        seed = "".join(chars)
+        if len(seed) == 5:
+            font_size = 100
+        else:
+            font_size = 120
+        for i in range(repeat):
+            im = image.generate_dbg(str(seed), font_size=font_size)
+#         im.show()
+            im.save(out_dir+seed+"_"+str(i) + ".png", format="png")
+fire.Fire()
+# gen(2)    
+    
+    
+    
+    
